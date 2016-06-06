85e6220b334a44e47faed01792465cdca79c164d
xuxiang1 <xuxiang1@letv.com>
2016-06-06 16:32:10 +0800

    Title: seconde commit Description:

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,8 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
+J1=(X*theta-y).^2;
+J=sum(J1(:))/2/m;
 
 
 % =========================================================================
